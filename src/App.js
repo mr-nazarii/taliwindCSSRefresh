70c1d212556cb6f5de390ede7249eb087ad0307d
Nazarii <nazariikubik@gmail.com>
2022-11-16 14:11:16 -0500

    add tailwind-Css

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <divv className="max-w-4xl px-5 mx-auto">
+      <h1 className="">Hello world!</h1>
+    </divv>
+  );
+}
+
+export default App;
